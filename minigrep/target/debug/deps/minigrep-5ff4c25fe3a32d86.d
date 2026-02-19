b9bab42231200c97b25d45de3734829b0458ceef
zenithx01 <rahimudiin18@gmail.com>
2026-02-19 19:37:27 +0530

    getting the user input as cli argument

---
+++ minigrep/target/debug/deps/minigrep-5ff4c25fe3a32d86.d
@@ -0,0 +1,5 @@
+/home/rahim/Desktop/DEV-WORK/RUST-DUMP/PROJECTS/minigrep/target/debug/deps/libminigrep-5ff4c25fe3a32d86.rmeta: src/main.rs
+
+/home/rahim/Desktop/DEV-WORK/RUST-DUMP/PROJECTS/minigrep/target/debug/deps/minigrep-5ff4c25fe3a32d86.d: src/main.rs
+
+src/main.rs:
